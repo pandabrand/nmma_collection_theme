952515d799dfacb2250f09ffe6c28c050196d9cb
pandabrand <pandabrand@gmail.com>
2019-01-20 17:21:01 -0600

    addg block preprocess block to theme

--- template.php
+++ template.php
@@ -4,3 +4,20 @@
  * @file
  * The primary PHP file for this theme.
  */
+
+function nmma_collection_theme_preprocess_block(&$variables) {
+  $variables['theme_hook_suggestions'][] = 'block__' . $variables['block']->region;
+  $variables['theme_hook_suggestions'][] = 'block__' . $variables['block']->module;
+  $variables['theme_hook_suggestions'][] = 'block__' . $variables['block']->delta;
+
+  // Add block description as template suggestion
+  $block = block_custom_block_get($variables['block']->delta);
+
+  // Transform block description to a valid machine name
+  if (!empty($block['info'])) {
+    setlocale(LC_ALL, 'en_US');
+
+    // required for iconv()
+    $variables['theme_hook_suggestions'][] = 'block__' . str_replace(' ', '_', strtolower(iconv('UTF-8', 'ASCII//TRANSLIT', $block['info'])));
+  }
+}
