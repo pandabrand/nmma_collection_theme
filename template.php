193641f736479a3b2ad6fa52736aed7af9333981
pandabrand <pandabrand@gmail.com>
2019-01-25 15:07:01 -0500

    add page override

--- template.php
+++ template.php
@@ -4,8 +4,13 @@
  * @file
  * The primary PHP file for this theme.
  */
-function nmma_collection_theme_preprocess_page(&$vars) {
+function nmma_collection_theme_preprocess_page(&$variables) {
   // var_dump($vars['theme_hook_suggestions']);
+  if (isset($variables['node']->type)) {
+    // If the content type's machine name is "my_machine_name" the file
+    // name will be "page--my-machine-name.tpl.php".
+    $variables['theme_hook_suggestions'][] = 'page__' . $variables['node']->type;
+  }
 }
 
 function nmma_collection_theme_preprocess_block(&$variables) {
