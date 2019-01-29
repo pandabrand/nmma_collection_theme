895bb70988afdabec5e6a4db0ca08fc0926c9cfc
pandabrand <pandabrand@gmail.com>
2019-01-28 23:06:19 -0600

    trying drupal jquery example.

--- js/nmma_main.js
+++ js/nmma_main.js
@@ -1,4 +1,10 @@
-var search_element = document.getElementById('edit-combine');
-if(search_element) {
-  search_element.placeholder = 'Discover by Artist Name, Title, or Keyword';
+// var search_element = document.getElementById('edit-combine');
+// if(search_element) {
+//   search_element.placeholder = 'Discover by Artist Name, Title, or Keyword';
+// }
+
+if (Drupal.jsEnabled) {
+  $(document).ready(function(){
+    $('form#views-exposed-form-nmma-collection-view-page-2 input.form-text').example('Discover by Artist Name, Title, or Keyword', {className: 'search_example'});
+  });
 }
